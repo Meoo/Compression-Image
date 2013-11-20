68eb032dcf46c7a817b2c2fb757ea42a6cb049dc
Bouchon <knoblauch.florian@gmail.com>
2013-11-20 14:28:10 +0100

    Create Image.h

---
+++ src/Image.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+#include "Pixel.h"
+
+class Image
+{
+public:
+	Image(IplImage* img);
+
+	int getWidth(void);
+	int getHeight(void);
+	int getNbChannels(void);
+	Pixel getPixel(int x, int y);
+	IplImage* getIplImage(void);
+	int getSize();
+
+	void setPixel(Pixel pixel, int x, int y);
+
+	~Image();
+
+private:
+	int width, height, nbChannels;
+	IplImage* image;
+	Pixel **pixels;
+};
