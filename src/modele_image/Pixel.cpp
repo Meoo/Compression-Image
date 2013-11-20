ddb6f0cb239529770e01db08b418f07a0e2580e5
Bouchon <knoblauch.florian@gmail.com>
2013-11-20 18:07:06 +0100

    Update Pixel.cpp

--- src/modele_image/Pixel.cpp
+++ src/modele_image/Pixel.cpp
@@ -1,27 +1,43 @@
 #include <opencv2\core\core.hpp>
 #include <opencv2\highgui\highgui.hpp>
 
-#include "modele_image/Pixel.h"
+#include "Pixel.h"
 
 
 Pixel::Pixel() {
 	Pixel(0, 0, 0);
 }
 
-Pixel::Pixel(uchar r, uchar v, uchar b)
+Pixel::Pixel(int r, int v, int b)
 {
 	this->r = r;
 	this->v = v;
 	this->b = b;
 }
 
-uchar Pixel::getR() { return r; }
-uchar Pixel::getV() { return v; }
-uchar Pixel::getB() { return b; }
+int Pixel::getR() { return r; }
+int Pixel::getV() { return v; }
+int Pixel::getB() { return b; }
 
-void Pixel::setR(uchar r) { this->r = r; }
-void Pixel::setV(uchar v) { this->v = v; }
-void Pixel::setB(uchar b) { this->b = b; }
+void Pixel::setR(int r) { this->r = r; }
+void Pixel::setV(int v) { this->v = v; }
+void Pixel::setB(int b) { this->b = b; }
+
+Pixel Pixel::sub(Pixel p) {
+	return Pixel(this->r - p.getR(), this->v - p.getV(), this->b - p.getB());
+}
+
+Pixel Pixel::add(Pixel p) {
+	return Pixel(this->r + p.getR(), this->v + p.getV(), this->b + p.getB());
+}
+
+Pixel Pixel::div(Pixel p) {
+	return Pixel(this->r / p.getR(), this->v / p.getV(), this->b / p.getB());
+}
+
+Pixel Pixel::mul(Pixel p) {
+	return Pixel(this->r * p.getR(), this->v * p.getV(), this->b * p.getB());
+}
 
 Pixel::~Pixel()
 {
