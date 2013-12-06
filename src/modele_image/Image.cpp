9ed7049348b54706308a3b25e9b8926f656e893f
Bouchon <knoblauch.florian@gmail.com>
2013-12-06 15:24:56 +0100

    Update Image.cpp

--- src/modele_image/Image.cpp
+++ src/modele_image/Image.cpp
@@ -2,9 +2,27 @@
 #include <opencv2\core\core.hpp>
 #include <opencv2\highgui\highgui.hpp>
 
-#include "modele_image/Image.h"
+#include "Image.h"
 
 using namespace std;
+
+Image::Image(int width, int height) {
+	this->image = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 3);
+	this->width = width;
+	this->height = height;
+	this->nbChannels = 3;
+
+	CvScalar pixel;
+	pixels = new Pixel*[width];
+
+	// Matrice de pixels vide
+	for (int i = 0; i < width; i++) {
+		pixels[i] = new Pixel[height];
+		for (int j = 0; j < height; j++) {
+			setPixel(Pixel(0, 0, 0), i, j);
+		}
+	}
+}
 
 Image::Image(IplImage* img) {
 	
@@ -23,7 +41,7 @@ Image::Image(IplImage* img) {
 
 		for (int j = 0; j < height; j++) {
 
-			CvScalar pix = cvGet2D(img, i, j);
+			CvScalar pix = cvGet2D(img, j, i);
 			pixels[i][j] = Pixel(pix.val[2], pix.val[1], pix.val[0]);
 		}
 	}
@@ -39,15 +57,15 @@ void Image::setPixel(Pixel pixel, int x, int y) { pixels[x][y] = pixel; }
 
 IplImage* Image::getIplImage() {
 	
+	CvScalar value;
 	for (int i = 0; i < width; i++) {
 
 		for (int j = 0; j < height; j++) {
 			
-			CvScalar value;
-			value.val[2] = (double)pixels[i][j].getR();
-			value.val[1] = (double)pixels[i][j].getV();
-			value.val[0] = (double)pixels[i][j].getB();
-			cvSet2D(image, i, j, value);
+			value.val[2] = pixels[i][j].getR();
+			value.val[1] = pixels[i][j].getV();
+			value.val[0] = pixels[i][j].getB();
+			cvSet2D(image, j, i, value);
 		}
 	}
 	return this->image;
